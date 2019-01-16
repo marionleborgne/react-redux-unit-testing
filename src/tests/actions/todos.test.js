06a63b053a598a0c9b83b1fcfea83162511ee926
Marion Le Borgne <marion.lb@uber.com>
2019-01-15 17:41:18 -0800

    example of store mocking

--- src/tests/actions/todos.test.js
+++ src/tests/actions/todos.test.js
@@ -1,5 +1,9 @@
 import * as actions from '../../actions'
 import types from '../../constants/actionTypes'
+import createMockStore from 'redux-mock-store';
+
+const middlewares = [];
+const mockStore = createMockStore(middlewares);
 
 describe('actions', () => {
 
@@ -14,15 +18,20 @@ describe('actions', () => {
   });
 
   it('should create an action to toggle a todo', () => {
+
+    // Example of full store mocking.
+    // NOTE: mocking the whole store is necessary
+    // for async actions, like when using thunk. Read more:
+    // https://github.com/reduxjs/redux/blob/master/docs/
+    // recipes/WritingTests.md#async-action-creators
+    const defaultState = {todos: []};
+    const store = mockStore(defaultState);
+
     const expectedAction = {
       type: types.TOGGLE_TODO,
       id: 'test'
     };
-    const actualAction = actions.toggleTodo(expectedAction.id);
-    expect(actualAction).toEqual(expectedAction);
+    store.dispatch(actions.toggleTodo(expectedAction.id));
+    expect(store.getActions()).toEqual([expectedAction]);
   });
-
-  // NOTE: for async actions, like when using thunk, read:
-  // https://github.com/reduxjs/redux/blob/master/docs/
-  // recipes/WritingTests.md#async-action-creators
 });
